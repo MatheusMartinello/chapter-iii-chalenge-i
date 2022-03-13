5dda5e5e38cb35263131b3ffa4641630f9d4c1ed
Matheus Martinello <matheusfontoura0@gmail.com>
2022-03-13 12:01:22 -0300

    SecondCommit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
 import { GetStaticProps } from 'next';
+import { Head } from 'next/document';
 
+import { TiCalendarOutline } from 'react-icons/ti';
+import { MdOutlinePersonOutline } from 'react-icons/md';
+import Link from 'next/link';
+import ptBR from 'date-fns/locale/pt-BR';
+import { format } from 'date-fns';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,9 +30,54 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  const date = format(new Date(), ' dd LLL yyyy', {
+    locale: ptBR,
+  });
+  return (
+    <>
+      <div className={styles.conteiner}>
+        <div>
+          <Link key={1} href={`/posts/1`}>
+            <a key={1}>
+              <strong>Como utilizar Hooks</strong>
+              <p>Pensando em sincronização em vez de ciclos de vida.</p>
+              <div className={styles.content}>
+                <p>
+                  <TiCalendarOutline className={styles.icon} /> {date}
+                </p>
+                <p style={{ marginLeft: 15 }}>
+                  <MdOutlinePersonOutline className={styles.icon} /> Nome Autor
+                </p>
+              </div>
+            </a>
+          </Link>
+        </div>
+      </div>
+      <div className={styles.conteiner}>
+        <div>
+          <Link key={2} href={`/posts/1`}>
+            <a key={2}>
+              <strong>Criando um app CRA do Zero</strong>
+              <p>Pensando em sincronização em vez de ciclos de vida.</p>
+              <div className={styles.content}>
+                <p>
+                  <TiCalendarOutline className={styles.icon} /> {date}
+                </p>
+                <p style={{ marginLeft: 15 }}>
+                  <MdOutlinePersonOutline className={styles.icon} /> Nome Autor
+                </p>
+              </div>
+            </a>
+          </Link>
+        </div>
+      </div>
+      <div className={styles.buttonContent}>
+        <button>Carregar Mais</button>
+      </div>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
